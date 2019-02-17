f075cd8075d14f89b0c6daac6238514c717aadbb
ricardo95104 <jose.maya@epn.edu.ec>
2019-02-17 08:39:47 -0500

    login
    Validad solo letras

--- BackEnd/backEnd/api/controllers/UsuarioController.js
+++ BackEnd/backEnd/api/controllers/UsuarioController.js
@@ -6,7 +6,20 @@
  */
 
 module.exports = {
-  
 
+  login: async (req, res) => {
+    const parametros = req.allParams();
+    if(!parametros.nombre.match(/^[a-zA-z\s]+$/))
+      return res.badRequest({mensaje:'Usuario Invalido, ingrese solo letras'});
+    var usuarioLogeado = await Usuario.find({
+      nombre: parametros.nombre,
+      password: parametros.password,
+    });
+    if(usuarioLogeado){
+      return res.ok(usuarioLogeado);
+    }else{
+      return res.badRequest({mensaje:'Error'});
+    }
+  }
 };
 
